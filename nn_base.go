f94407c6af2de029fd730cde0065effaecf00b1e
Rohan Raja <rohanraja9@gmail.com>
2015-09-05 21:13:15 +0530

    Added NN Base interface for all generic ML Algorithms

---
+++ nn_base.go
@@ -0,0 +1,43 @@
+package nnet
+
+type NN_Base interface {
+	Compute_Cost(Params, Dataset) float64
+	Compute_Gradient(Params, Dataset) Params
+
+	GetParams() Params
+	GetDataset() Dataset
+}
+
+func GetUnpackedFunctions(nn NN_Base) (f func([]float64) float64, fprime func([]float64) []float64) {
+
+	params := nn.GetParams()
+	dataset := nn.GetDataset()
+	f = func(x []float64) float64 {
+
+		var p Params
+		p.Pack(x, params.GetPinfo())
+
+		return nn.Compute_Cost(p, dataset)
+	}
+	fprime = func(x []float64) []float64 {
+
+		var p Params
+		p.Pack(x, params.GetPinfo())
+
+		grads := nn.Compute_Gradient(p, dataset)
+
+		return grads.UnPack()
+	}
+
+	return
+}
+
+func GradientCheck(nn NN_Base) bool {
+
+	f, fprime := GetUnpackedFunctions(nn)
+	params := nn.GetParams()
+	x := params.UnPack()
+
+	return GradCheckGeneral(f, fprime, x)
+
+}
